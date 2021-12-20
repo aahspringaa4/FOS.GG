dc37ce580df5807594ded14efbb4639e52355058
유현명 <aahspringaa4@naver.com>
2021-12-21 00:50:05 +0900

    Feat :: 최신 메타 검색 화면 구현

--- app/src/main/java/com/example/fifausersearchservice/feature/meta/MetaFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/meta/MetaFragment.kt
@@ -1,4 +1,27 @@
 package com.example.fifausersearchservice.feature.meta
 
-class MetaFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fifausersearchservice.R
+import com.example.fifausersearchservice.base.BaseFragment
+import com.example.fifausersearchservice.databinding.MatchsearchBinding
+import com.example.fifausersearchservice.databinding.MetaBinding
+
+class MetaFragment: BaseFragment<MetaBinding>(
+    R.layout.meta
+){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return binding.root
+    }
+
+    override fun observeEvent() {
+
+    }
+
+    override fun initView() {
+
+    }
+
 }
