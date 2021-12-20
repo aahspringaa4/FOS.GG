455eb3a3edd4f0429d83f06711e212110d13bf4b
유현명 <aahspringaa4@naver.com>
2021-12-21 00:52:05 +0900

    Feat :: 유저 검색 Fragment DataBInding 등록

--- app/src/main/java/com/example/fifausersearchservice/feature/user/UserFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/user/UserFragment.kt
@@ -1,4 +1,27 @@
 package com.example.fifausersearchservice.feature.user
 
-class UserFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fifausersearchservice.R
+import com.example.fifausersearchservice.base.BaseFragment
+import com.example.fifausersearchservice.databinding.MetaBinding
+import com.example.fifausersearchservice.databinding.UserBinding
+
+class UserFragment: BaseFragment<UserBinding>(
+    R.layout.user
+){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return null
+    }
+
+    override fun observeEvent() {
+
+    }
+
+    override fun initView() {
+
+    }
+
 }
