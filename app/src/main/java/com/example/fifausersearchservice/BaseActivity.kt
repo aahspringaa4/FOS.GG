cf001b3146838377233c51f66c4f6e2fa4227898
유현명 <aahspringaa4@naver.com>
2021-12-20 11:54:06 +0900

    Feat :: BaseActivity DataBinding 등록 및 observeEvent 생성

--- app/src/main/java/com/example/fifausersearchservice/BaseActivity.kt
+++ app/src/main/java/com/example/fifausersearchservice/BaseActivity.kt
@@ -2,10 +2,29 @@ package com.example.fifausersearchservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
-class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<B: ViewDataBinding> (
+    @LayoutRes private val layoutId: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+
+        observeEvent()
     }
+
+    fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    abstract fun observeEvent()
 }
