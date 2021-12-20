85ef53ec8befc80e2b111a0cad6de2c8fc3a1e7b
유현명 <aahspringaa4@naver.com>
2021-12-21 00:50:20 +0900

    Feat :: 상위 랭커 화면 DataBinding 구현

--- app/src/main/java/com/example/fifausersearchservice/feature/rank/TopRankFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/rank/TopRankFragment.kt
@@ -1,4 +1,27 @@
 package com.example.fifausersearchservice.feature.rank
 
-class TopRankFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fifausersearchservice.R
+import com.example.fifausersearchservice.base.BaseFragment
+import com.example.fifausersearchservice.databinding.ActivityToprankBinding
+
+class TopRankFragment: BaseFragment<ActivityToprankBinding>(
+    R.layout.activity_toprank
+){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return binding.root
+    }
+
+    override fun observeEvent() {
+
+    }
+
+    override fun initView() {
+
+    }
+
 }
