f7349000ee43d38a33a38be883fe771c59c37a58
유현명 <aahspringaa4@naver.com>
2021-12-20 19:25:06 +0900

    Feat :: DI Koin 모듈 추가

--- app/src/main/java/com/example/fifausersearchservice/di/main/MainModule.kt
+++ app/src/main/java/com/example/fifausersearchservice/di/main/MainModule.kt
@@ -1,2 +1,11 @@
 package com.example.fifausersearchservice.di.main
 
+import com.example.fifausersearchservice.data.main.MainRepository
+import com.example.fifausersearchservice.feature.main.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val mainModule = module{
+    single{ MainRepository() }
+    viewModel { MainViewModel (get()) }
+}
