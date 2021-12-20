46cb9da0f641b179455acdc58a0115eadf504914
유현명 <aahspringaa4@naver.com>
2021-12-20 11:54:20 +0900

    Feat :: BaseActivity 연동

--- app/src/main/java/com/example/fifausersearchservice/MainActivity.kt
+++ app/src/main/java/com/example/fifausersearchservice/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.fifausersearchservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fifausersearchservice.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    override fun observeEvent() {
+
     }
 }
