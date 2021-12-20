955f0185ed9067450418c87ec56ec5bb91e9912b
유현명 <aahspringaa4@naver.com>
2021-12-21 00:52:25 +0900

    Feat :: 메인 화면 Fragment 구현

--- app/src/main/java/com/example/fifausersearchservice/feature/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/main/ui/MainActivity.kt
@@ -5,7 +5,12 @@ import com.example.fifausersearchservice.R
 import com.example.fifausersearchservice.base.BaseActivity
 import com.example.fifausersearchservice.databinding.ActivityMainBinding
 import android.content.Intent
+import android.util.Log
 import com.example.fifausersearchservice.feature.main.viewmodel.MainViewModel
+import com.example.fifausersearchservice.feature.matchsearch.MatchSearchFragment
+import com.example.fifausersearchservice.feature.meta.MetaFragment
+import com.example.fifausersearchservice.feature.rank.TopRankFragment
+import com.example.fifausersearchservice.feature.user.UserFragment
 import org.koin.androidx.viewmodel.compat.ScopeCompat.viewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -29,9 +34,45 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
         binding.btSearch.setOnClickListener {
 
         }
+
+        setFrag(0)
+
+        binding.fgSearch.setOnClickListener{
+            setFrag(0)
+        }
+
+        binding.fgMatch.setOnClickListener {
+            setFrag(1)
+        }
+
+        binding.fgMeta.setOnClickListener {
+            setFrag(2)
+        }
+
+        binding.fgRank.setOnClickListener {
+            setFrag(3)
+        }
     }
 
     override fun observeEvent() {
 
+    }
+
+    private fun setFrag(fragnum : Int) {
+        val ft = supportFragmentManager.beginTransaction()
+        when(fragnum){
+            0 -> {
+                ft.replace(R.id.fragment, UserFragment()).commit()
+            }
+            1 -> {
+                ft.replace(R.id.fragment, MatchSearchFragment()).commit()
+            }
+            2 -> {
+                ft.replace(R.id.fragment, MetaFragment()).commit()
+            }
+            3 -> {
+                ft.replace(R.id.fragment, TopRankFragment()).commit()
+            }
+        }
     }
 }
