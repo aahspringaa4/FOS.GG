dd37ec8d62a88dcfd066683d23db4cbebb14ce59
유현명 <aahspringaa4@naver.com>
2021-12-20 17:28:49 +0900

    Feat :: network provider 구현

--- app/src/main/java/com/example/fifausersearchservice/data/ApiProvider.kt
+++ app/src/main/java/com/example/fifausersearchservice/data/ApiProvider.kt
@@ -1,4 +1,12 @@
 package com.example.fifausersearchservice.data
 
-class ApiProvider {
-}
+import BASE_URL
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+private val retrofit: Retrofit = Retrofit.Builder().apply {
+    baseUrl(BASE_URL)
+    addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+    addConverterFactory(GsonConverterFactory.create())
+}.build()
