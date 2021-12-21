ce3b2674a0e3718892a5c3e2d29f7c025d5b3046
유현명 <aahspringaa4@naver.com>
2021-12-21 10:39:07 +0900

    Feat :: User 조회 Fragment 구현

--- app/src/main/java/com/example/fifausersearchservice/feature/user/UserFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/user/UserFragment.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.fifausersearchservice.R
 import com.example.fifausersearchservice.base.BaseFragment
 import com.example.fifausersearchservice.databinding.MetaBinding
 import com.example.fifausersearchservice.databinding.UserBinding
+import com.example.fifausersearchservice.feature.main.ui.MainActivity
+import com.example.fifausersearchservice.feature.main.viewmodel.MainViewModel
+import com.example.fifausersearchservice.feature.user.dto.ResponseUserDTO
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class UserFragment: BaseFragment<UserBinding>(
     R.layout.user
 ){
+    private val vm: MainViewModel by viewModel()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return null
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.user, container, false)
+
+        return binding.root
     }
 
     override fun observeEvent() {
@@ -24,4 +34,8 @@ class UserFragment: BaseFragment<UserBinding>(
 
     }
 
+    fun userSearch(){
+        binding.tvName.text = vm.name
+        binding.tvLevel.text = vm.level.toString()
+    }
 }
