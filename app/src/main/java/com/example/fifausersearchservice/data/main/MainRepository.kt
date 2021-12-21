dec5c6c47b83e7e80f2fa467cd0ddd403d608692
유현명 <aahspringaa4@naver.com>
2021-12-21 09:59:06 +0900

    Feat :: 비동기적 Repository 구현

--- app/src/main/java/com/example/fifausersearchservice/data/main/MainRepository.kt
+++ app/src/main/java/com/example/fifausersearchservice/data/main/MainRepository.kt
@@ -1,9 +1,17 @@
 package com.example.fifausersearchservice.data.main
 
-import com.example.fifausersearchservice.feature.main.viewmodel.MainViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
+import com.example.fifausersearchservice.data.mainApi
+import com.example.fifausersearchservice.feature.user.dto.ResponseUserDTO
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Call
+import retrofit2.Response
 
 class MainRepository{
-
+    fun userPost(nickname: String) : @NonNull Single<Response<ResponseUserDTO>> =
+        mainApi.createUserPost(nickname)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
