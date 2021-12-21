2172af9e27d4ac7aa7024c7cc17410199f538118
유현명 <aahspringaa4@naver.com>
2021-12-21 15:58:18 +0900

    Feat :: fragment databinding 적용

--- app/src/main/java/com/example/fifausersearchservice/feature/matchsearch/MatchSearchFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/matchsearch/MatchSearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.fifausersearchservice.R
 import com.example.fifausersearchservice.base.BaseFragment
 import com.example.fifausersearchservice.databinding.MatchsearchBinding
@@ -12,6 +13,8 @@ class MatchSearchFragment: BaseFragment<MatchsearchBinding>(
     R.layout.matchsearch
 ){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.matchsearch, container, false)
+
         return binding.root
     }
 
