cc73549cf8952aa1a2c3231bf72117123e910b0e
유현명 <aahspringaa4@naver.com>
2021-12-21 15:58:24 +0900

    Feat :: fragment databinding 적용

--- app/src/main/java/com/example/fifausersearchservice/feature/meta/MetaFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/meta/MetaFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.fifausersearchservice.R
 import com.example.fifausersearchservice.base.BaseFragment
 import com.example.fifausersearchservice.databinding.MatchsearchBinding
@@ -13,6 +14,8 @@ class MetaFragment: BaseFragment<MetaBinding>(
     R.layout.meta
 ){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.meta, container, false)
+
         return binding.root
     }
 
