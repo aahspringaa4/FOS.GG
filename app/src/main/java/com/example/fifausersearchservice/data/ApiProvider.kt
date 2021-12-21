1897a53ec5747ac988efb6d43a5246af2d4fbf13
유현명 <aahspringaa4@naver.com>
2021-12-21 09:58:41 +0900

    Feat :: mainApi Provider에 연동

--- app/src/main/java/com/example/fifausersearchservice/data/ApiProvider.kt
+++ app/src/main/java/com/example/fifausersearchservice/data/ApiProvider.kt
@@ -1,6 +1,7 @@
 package com.example.fifausersearchservice.data
 
 import BASE_URL
+import com.example.fifausersearchservice.data.main.MainApi
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,3 +11,7 @@ private val retrofit: Retrofit = Retrofit.Builder().apply {
     addCallAdapterFactory(RxJava3CallAdapterFactory.create())
     addConverterFactory(GsonConverterFactory.create())
 }.build()
+
+val mainApi : MainApi by lazy {
+    retrofit.create(MainApi::class.java)
+}
