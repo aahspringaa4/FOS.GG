e34b8bdf65dcaa00274daea7d8c4b0bf0f061100
유현명 <aahspringaa4@naver.com>
2021-12-22 15:03:56 +0900

    Feat :: 상속 값 중복 제거

--- app/src/main/java/com/example/fifausersearchservice/feature/rank/TopRankFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/rank/TopRankFragment.kt
@@ -1,23 +1,12 @@
 package com.example.fifausersearchservice.feature.rank
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import com.example.fifausersearchservice.R
 import com.example.fifausersearchservice.base.BaseFragment
-import com.example.fifausersearchservice.databinding.ToprankBinding
+import com.example.fifausersearchservice.databinding.FragmentToprankBinding
 
-class TopRankFragment: BaseFragment<ToprankBinding>(
-    R.layout.toprank
+class TopRankFragment: BaseFragment<FragmentToprankBinding>(
+    R.layout.fragment_toprank
 ){
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.toprank, container, false)
-
-        return binding.root
-    }
 
     override fun observeEvent() {
 
