ba7cbee25374ee223c0c13760df91f964867fe75
유현명 <aahspringaa4@naver.com>
2021-12-22 15:03:25 +0900

    Feat :: onCreateView 삭제

--- app/src/main/java/com/example/fifausersearchservice/feature/matchsearch/MatchSearchFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/matchsearch/MatchSearchFragment.kt
@@ -7,16 +7,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.fifausersearchservice.R
 import com.example.fifausersearchservice.base.BaseFragment
-import com.example.fifausersearchservice.databinding.MatchsearchBinding
+import com.example.fifausersearchservice.databinding.FragmentMatchsearchBinding
 
-class MatchSearchFragment: BaseFragment<MatchsearchBinding>(
-    R.layout.matchsearch
+class MatchSearchFragment: BaseFragment<FragmentMatchsearchBinding>(
+    R.layout.fragment_matchsearch
 ){
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.matchsearch, container, false)
-
-        return binding.root
-    }
 
     override fun observeEvent() {
 
