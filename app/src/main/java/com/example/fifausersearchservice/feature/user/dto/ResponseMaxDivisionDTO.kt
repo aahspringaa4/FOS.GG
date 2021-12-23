2f15b917c0844e55b56117e76870e971ccc316d3
유현명 <aahspringaa4@naver.com>
2021-12-23 11:42:38 +0900

    Feat :: 최대 기록 DTO Response값 구현

--- app/src/main/java/com/example/fifausersearchservice/feature/user/dto/ResponseMaxDivisionDTO.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/user/dto/ResponseMaxDivisionDTO.kt
@@ -1,3 +1,7 @@
 package com.example.fifausersearchservice.feature.user.dto
 
-data class ResponseMaxDivisionDTO()
+data class ResponseMaxDivisionDTO(
+    val matchType: Int,
+    val division: Int,
+    val achievementDate: String
+)
