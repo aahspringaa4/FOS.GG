ba5bd9ed49325b5e27978a377213a85441be5759
유현명 <aahspringaa4@naver.com>
2021-12-24 11:19:16 +0900

    Feat :: 공식 경기 종류 구분

--- app/src/main/java/com/example/fifausersearchservice/feature/user/UserFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/user/UserFragment.kt
@@ -11,17 +11,26 @@ class UserFragment: BaseFragment<FragmentUsersBinding>(
     R.layout.fragment_users
 ){
     private val vm: MainViewModel by sharedViewModel()
+
     override fun observeEvent() {
         vm.success.observe(viewLifecycleOwner, {
             userSearch()
         })
     }
 
-    override fun initView() {
-
+    override fun initView() { // onCreate
     }
 
-    fun userSearch(){
+    private fun userSearch(){
+        val data = vm.matchType
+        when(data) {
+            50 -> binding.bestgrade.visibility = View.VISIBLE
+
+            52 -> binding.coact.visibility = View.VISIBLE
+
+            54 -> binding.volta.visibility = View.VISIBLE
+        }
+
         binding.tvName.text = vm.name
         binding.tv.visibility = View.VISIBLE
         binding.tvLevel.text = vm.level.toString()
