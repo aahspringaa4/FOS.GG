83441db601786fc6fb6f03b7a8df9d02f0010fda
유현명 <aahspringaa4@naver.com>
2021-12-24 14:13:06 +0900

    Feat :: Response Division Fifa Api에 맞게 구별

--- app/src/main/java/com/example/fifausersearchservice/feature/user/UserFragment.kt
+++ app/src/main/java/com/example/fifausersearchservice/feature/user/UserFragment.kt
@@ -22,13 +22,50 @@ class UserFragment: BaseFragment<FragmentUsersBinding>(
     }
 
     private fun userSearch(){
-        val data = vm.matchType
-        when(data) {
+        when(vm.matchType) {
             50 -> binding.bestgrade.visibility = View.VISIBLE
 
             52 -> binding.coact.visibility = View.VISIBLE
 
-            54 -> binding.volta.visibility = View.VISIBLE
+            214 -> binding.volta.visibility = View.VISIBLE
+        }
+
+        when(vm.division){
+            800 -> binding // 슈퍼 챔피언
+
+            900 -> binding // 챔피언스
+
+            1000 -> binding // 슈퍼챌린지
+
+            1100 -> binding // 챌린지1
+
+            1200 -> binding // 챌린지2
+
+            1300 -> binding // 챌린지3
+
+            2000 -> binding // 월드클래스1
+
+            2100 -> binding // 월드클래스2
+
+            2200 -> binding // 월드클래스3
+
+            2300 -> binding // 프로1
+
+            2400 -> binding // 프로2
+
+            2500 -> binding // 프로3
+
+            2600 -> binding // 세미프로1
+
+            2700 -> binding // 세미프로2
+
+            2800 -> binding // 세미프로3
+
+            2900 -> binding // 유망주1
+
+            3000 -> binding // 유망주2
+
+            3100 -> binding // 유망주3
         }
 
         binding.tvName.text = vm.name
