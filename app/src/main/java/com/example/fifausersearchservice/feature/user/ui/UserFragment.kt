9bf7aa6877f67777c6027d50f058c275ef3c4564
유현명 <aahspringaa4@naver.com>
2021-12-27 10:37:23 +0900

    Remove :: 기존 코드 제거

---
+++ app/src/main/java/com/example/fifausersearchservice/feature/user/ui/UserFragment.kt
@@ -0,0 +1,44 @@
+package com.example.fifausersearchservice.feature.user.ui
+
+import android.view.View
+import com.example.fifausersearchservice.R
+import com.example.fifausersearchservice.base.BaseFragment
+import com.example.fifausersearchservice.databinding.FragmentUsersBinding
+import com.example.fifausersearchservice.feature.main.viewmodel.MainViewModel
+import com.example.fifausersearchservice.feature.user.RankListViewData
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+class UserFragment: BaseFragment<FragmentUsersBinding>(
+    R.layout.fragment_users
+){
+    private val vm: MainViewModel by sharedViewModel()
+    var rankList = arrayListOf<RankListViewData>()
+
+    override fun observeEvent() {
+        vm.success.observe(viewLifecycleOwner, {
+            userSearch()
+        })
+    }
+
+    override fun initView() { // onCreate
+    }
+
+    private fun userSearch(){
+        when(vm.matchType) {
+            50 -> {
+                binding.top.visibility = View.VISIBLE
+
+                binding.tvTopRank.text = vm.division.name
+            }
+
+            52 -> binding.coach.visibility = View.VISIBLE
+
+            214 -> binding.volta.visibility = View.VISIBLE
+        }
+
+        binding.tvName.text = vm.name
+        binding.tv.visibility = View.VISIBLE
+        binding.tvLevel.text = vm.level.toString()
+        binding.textView.visibility = View.VISIBLE
+    }
+}
