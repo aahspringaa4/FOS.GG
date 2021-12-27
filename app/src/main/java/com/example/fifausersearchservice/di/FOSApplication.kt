d6626f88d654e534aade5f854946ef5ed13ef92a
유현명 <aahspringaa4@naver.com>
2021-12-27 15:39:23 +0900

    Feat :: DI Koin -> Hilt Application 변경

--- app/src/main/java/com/example/fifausersearchservice/di/FOSApplication.kt
+++ app/src/main/java/com/example/fifausersearchservice/di/FOSApplication.kt
@@ -1,22 +1,9 @@
 package com.example.fifausersearchservice.di
 
 import android.app.Application
-import com.example.fifausersearchservice.di.module.mainModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class FOSApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
 
-        startKoin {
-            androidContext(this@FOSApplication)
-
-            modules(
-                listOf(
-                    mainModule
-                )
-            )
-        }
-    }
 }
